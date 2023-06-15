40291c2a52c5e48b050b6b8f08eccb9faa079469
cansoy <cansoymuhammed@hotmail.com>
2023-06-15 14:36:50 +0300

    location

--- public/js/c_shiftTraceLocationDetail.js
+++ public/js/c_shiftTraceLocationDetail.js
@@ -46,8 +46,8 @@ UI.shiftTraceLocationDetails.forEach(item=>{
                         <br>
                         <p></p>
                         <hr>
-                        <a href="https://www.google.com/maps/dir/@${carLatitute},${carLongtitute}" target="_blank">
-                            Araç EnlemBoylama Git
+                        <a href="https://www.google.com/search?q=${carLatitute},${carLongtitute}" target="_blank">
+                            Araç EnlemBoylama Git 1
                             <img class="icon-tedsuitwatch" src="../../svg/navigatelocation.svg">
                         </a>
                         <br>
