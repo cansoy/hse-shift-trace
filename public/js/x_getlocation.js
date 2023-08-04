10bd69a6b90438308ed707502836ac00e314196a
cansoy <cansoymuhammed@hotmail.com>
2023-08-04 16:03:58 +0300

    added accuracy

--- public/js/x_getlocation.js
+++ public/js/x_getlocation.js
@@ -5,10 +5,11 @@ navigator.geolocation.getCurrentPosition(
     (position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
+      const accuracy=position.coords.accuracy
       const a =document.createElement("a")
         a.href=`https://www.google.com/search?q=${latitude}%2C${longitude}&oq=${latitude}%2C${longitude}`
         a.target="_blank"
-        a.textContent=`Google da Yerimi Kontrol Et`
+        a.textContent=`Google da Yerimi Kontrol Et ( ${accuracy.toFixed(0)}mt )`
         location1.append(a)
     },
     (error) => {
